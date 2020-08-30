894271886cf31ae8a6e1f24178a87b31bda868e3
Muhammed Salih <37251794+Muhammed-Salih-Karademir@users.noreply.github.com>
2020-08-30 18:51:14 +0300

    Add files via upload
    Gstreamer için tüm paket kurumları listelenmiştir.

---
+++ installGstAll.sh
@@ -0,0 +1,43 @@
+# gstreamer
+sudo apt-get install gstreamer1.0-tools -y
+sudo apt-get install gstreamer1.0-omx -y
+sudo apt-get install gstreamer1.0-plugins-good -y
+sudo apt-get install gstreamer1.0-plugins-ugly -y
+sudo apt-get install gstreamer1.0-plugins-bad -y
+sudo apt-get install gstreamer1.0-alsa -y
+sudo apt-get install gstreamer1.0-plugins-base -y 
+sudo apt-get install gstreamer1.0-libav -y
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev -y
+sudo apt-get install automake unzip -y
+sudo apt-get install cmake git autoconf autopoint -y
+sudo apt-get install gtk-doc-tools glib-2.0 -y 
+sudo apt-get install bison flex libglib2.0-dev -y
+sudo apt-get install libunwind-dev libdw-dev libgtk-3-dev -y
+sudo apt-get install libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libtool libreadline-dev  -y
+
+# gst-plugins-base
+# theora
+sudo apt-get install libtheora-bin libtheora-dev libtheora-doc -y
+# vorbis
+sudo apt-get install libvorbis-dev -y
+# cdparanoia
+sudo apt-get install libcdparanoia-dev -y
+sudo apt-get install alsa-base alsa-tools -y
+sudo apt-get install libasound2-dev -y
+sudo apt-get install libopus-dev libvisual-0.4-dev libpango1.0-dev -y
+
+# gst-plugins-good
+sudo apt-get install libwavpack-dev libspeex-dev qt libjack-sdk libjpeg-dev libdv-dev libsoup2.4-dev qtdeclarative5-dev
+sudo apt-get install libcairo-dev -y
+
+#gst-plugins-bad
+sudo apt-get install x265 x264 -y
+# gst-plugins-ugly
+sudo apt-get install libx264-dev -y
+# gst-libav
+sudo apt-get install yasm nasm libbz2-dev liblzma-dev -y
+
+sudo apt install build-essential pkg-config libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev libtbb2 libtbb-dev libdc1394-22-dev -y
+
+
+
